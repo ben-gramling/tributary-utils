a5a8d7681f33b10f988a8017da5fdcb2535f7e01
Ben Gramling <bgramling18@gmail.com>
2024-06-15 16:23:03 -0500

    both sportsbooks fully operational

--- src/main/java/com/arbriver/tributaryutils/lib/model/Position.java
+++ src/main/java/com/arbriver/tributaryutils/lib/model/Position.java
@@ -4,10 +4,12 @@ import com.arbriver.tributaryutils.lib.model.constants.BetClass;
 import com.arbriver.tributaryutils.lib.model.constants.PositionStatus;
 import com.arbriver.tributaryutils.lib.utils.UpdateHelper;
 import lombok.Data;
+import lombok.NonNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.Instant;
+import java.util.Objects;
 
 @Data
 @Document(collection = "positions")
@@ -25,10 +27,9 @@ public class Position {
     private String participant;
 
     public void generateBetId() {
-        assert matchRefId != null;
-        assert value != null;
-        assert betType != null;
-        assert bookmaker != null;
-        this.betId = UpdateHelper.generateBetId(matchRefId, value, bookmaker, betType);
+        this.betId = UpdateHelper.generateBetId(Objects.requireNonNull(matchRefId),
+                Objects.requireNonNull(value),
+                Objects.requireNonNull(bookmaker),
+                Objects.requireNonNull(betType));
     }
 }
